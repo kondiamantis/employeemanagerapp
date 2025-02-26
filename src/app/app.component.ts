e1a0ae5369f10d440c07918abcf0b989831f7055
kdiamantis <kdiamantis@iknowhow.com>
2025-02-26 12:27:56 +0200

    Basic app with services and environments is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { Employee } from './interfaces/employee';
+import { EmployeeService } from './services/employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employeemanagerapp';
+
+  public employees: Employee[] | undefined;
+  
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
 }
