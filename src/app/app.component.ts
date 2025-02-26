70f27cd77d5170c1d482c26292cb6023a3bf629a
kdiamantis <kdiamantis@iknowhow.com>
2025-02-26 20:30:57 +0200

    Delete employee and search are working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent implements OnInit {
 
   public employees!: Employee[];
   public editEmployee : Employee | null = null;
+  public deleteEmployee : Employee | null = null;
   
   constructor(private employeeService: EmployeeService) { }
 
@@ -43,6 +44,7 @@ export class AppComponent implements OnInit {
       (response: Employee) => {
         console.log(response);
         this.getEmployees();
+        addForm.reset();
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
@@ -74,6 +76,23 @@ export class AppComponent implements OnInit {
     );
   }
 
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+          || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+          || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+          || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
+
   public onOpenModal(employee: Employee | null, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
@@ -89,6 +108,7 @@ export class AppComponent implements OnInit {
       button.setAttribute('data-target', '#updateEmployeeModal');
     }
     if (mode === 'delete') {
+      this.deleteEmployee = employee;
       button.setAttribute('data-target', '#deleteEmployeeModal');
     }
 
